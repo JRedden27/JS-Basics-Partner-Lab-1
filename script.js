9cb431de36c9c8d540555b8ff590b757be9a1c03
unknown <DBenson05@yahoo.com>
2020-11-17 19:08:08 -0500

    JS random

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let num1 = Number(prompt("Enter a number:"));
-let num2 = Number(prompt("Enter another number:"));
+//let num1 = Number(prompt("Enter a number:"));
+//let num2 = Number(prompt("Enter another number:"));
 
-let answer = (num1 / num2);
+//let answer = (num1 / num2);
 
-alert(answer);
+//alert(answer);
+
+let num3 = Math.floor((Math.random()* 10)+1);
+let guess = Number(prompt("Guess Number"));
+
+if(num3 === guess){
+    alert("Your Right!!!");
+} else{
+    alert("Your Wrong");
+}
